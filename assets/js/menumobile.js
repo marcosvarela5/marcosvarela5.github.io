ec38ab6a1809c3aadaae0630fcdfd2d5bccd4591
marcosvarela5 <marcosviesmugardos@gmail.com>
2025-06-30 19:49:36 +0200

    Main menu is now closing when navigating

--- assets/js/menumobile.js
+++ assets/js/menumobile.js
@@ -2,7 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
   const toggle = document.querySelector('.menu-toggle');
   const menu = document.getElementById('main-menu');
 
+  // Toggle del menú al hacer clic en el botón
   toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
+  });
+
+  // Cerrar el menú al hacer clic en cualquier enlace
+  const links = menu.querySelectorAll('a');
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('active');
+    });
   });
 });
